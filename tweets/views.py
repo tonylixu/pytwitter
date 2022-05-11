687715afee03cd24a3c180e37e40d003d0ddaf78
Tony Li Xu <tonylixu@gmail.com>
2022-05-10 21:55:58 -0400

    Use JsonResponse instead of HttpResponse

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -7,9 +7,28 @@ from .models import Tweet
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
-def tweet_detail_view(request,tweet_id, *args, **kwargs):
+def tweet_detail_view(request, tweet_id : int, *args, **kwargs):
+    """REST API VIEW
+    Consume by JavaScript or Swift
+    Args:
+        request (_type_): _description_
+        tweet_id (int): tweet id
+
+    Raises:
+        Http404: Page not found
+
+    Returns:
+        _type_: JSON data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+        data['message'] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status)
