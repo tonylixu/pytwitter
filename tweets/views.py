e5f7c8c69563d63901ff8f45273dd7f52bff75b0
Tony Li Xu <tonylixu@gmail.com>
2022-05-23 21:07:42 -0400

    Added first javascript

--- tweets/views.py
+++ tweets/views.py
@@ -8,6 +8,23 @@ def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "pages/home.html", context={}, status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+    """REST API VIEW
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
 def tweet_detail_view(request, tweet_id : int, *args, **kwargs):
     """REST API VIEW
     Consume by JavaScript or Swift
